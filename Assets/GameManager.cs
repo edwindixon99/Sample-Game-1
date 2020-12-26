581966d59a72033f31124a7cd223415878e2c770
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2020-12-26 15:03:38 +1300

    Imported the playlist scripts
    Added scripts from playlist. Made basic cube/plane map with movement in all directions

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿ using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        Debug.Log("You Won!"); 
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame ()
+    {
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            Debug.Log("game over");
+            Invoke("Restart", 1f);
+        }
+        
+    }
+
+    void Restart () 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 1000f;
+    public bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space")) {
+            jump = true;
+        } else {
+            jump = false;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        
+        
+
+        if (Input.GetKey("w")) {
+
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s")) {
+
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) {
+
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d")) {
+
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (jump) {
+            rb.AddForce(0, sidewaysForce * Time.deltaTime, 0);
+        }
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
